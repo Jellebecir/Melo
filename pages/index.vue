55230960c5a79d25e4743567865386ecaffe34bc
Jellebecir <jellebecir1999@gmail.com>
2025-04-21 14:16:03 +0200

    Removed redundant code from dashboard page

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,13 @@
 <template>
   <div>
     <div class="min-h-screen bg-gray-100 flex justify-center items-center">
-      <h1 class="text-4xl font-bold text-blue-500">Welcome to the Dashboard {{ user.email }}</h1>
+      <h1 class="text-4xl font-bold text-blue-500">Welcome to the Dashboard {{ avatar.name }}</h1>
     </div>
-    <UButton @click="logout" color="neutral" variant="outline">Logout</UButton>
   </div>
 </template>
 
 <script lang="ts" setup>
 
-const supabase = useSupabaseClient();
-const user = useSupabaseUser();
-
-const logout = async () => {
-  const { error } = supabase.auth.signOut();
-  if (error) {
-    console.error('Error signing out:', error.message);
-  } else {
-    // Redirect to the login page after successful logout
-    navigateTo('/login');
-  }
-};
+const avatar = await useAvatar();
 
 </script>
